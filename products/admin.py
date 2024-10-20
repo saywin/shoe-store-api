f6aa70386211786b71c627b31f0f04c5143b1527
tiway <stilon670@gmail.com>
2024-10-20 20:32:54 +0300

    Added miniature to product and category in admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from products.models import Category, Product, Gallery, Stock, Size
 
@@ -18,18 +19,41 @@ class StockInline(admin.TabularInline):
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ["name", "parent"]
+    list_display = ["get_picture", "name", "parent", "get_category_count"]
+    readonly_fields = ["get_picture"]
     prepopulated_fields = {"slug": ("name", )}
+
+    def get_picture(self, obj):
+        if obj.image:
+            return mark_safe(f"<img src='{obj.image.url}' width='50'>")
+        else:
+            return "---"
+
+    def get_category_count(self, obj):
+        if obj.products:
+            return str(len(obj.products.all()))
+
+    get_picture.short_description = "Мініатюра"
+    get_category_count.short_description = "Кількість товарів"
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ["pk", "name", "description", "price", "count", "color", "category", "created_at"]
+    list_display = ["get_image", "pk", "name", "description", "price", "count", "color", "category", "created_at"]
+    readonly_fields = ["get_image"]
     prepopulated_fields = {"slug": ("name", )}
     list_editable = ["price", "count", "color"]
     list_filter = ["price", "name"]
     list_display_links = ["pk", "name"]
     inlines = (GalleryInline, StockInline)
+
+    def get_image(self, obj):
+        if obj.images.all():
+            return mark_safe(f"<img src='{obj.images.all()[0].image.url}', width='70'>")
+        else:
+            return "---"
+
+    get_image.short_description = "Фото"
 
 
 @admin.register(Size)
